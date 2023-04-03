8ea8cdbea786902e02c81d2f95619aad0f07104c
Ryzeon <alex.ramon2004@gmail.com>
2023-04-02 22:51:38 -0500

    feat: screen finder init util

---
+++ src/main/java/me/ryzeon/mate/utils/ScreenFinder.java
@@ -0,0 +1,14 @@
+package me.ryzeon.mate.utils;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * Created by Ryzeon
+ * Project: ProjectoUPC_Mate
+ * Date: 4/2/23 @ 21:28
+ * Twitter: @Ryzeon_ 😎
+ * Github: github.ryzeon.me
+ */
+@UtilityClass
+public class ScreenFinder {
+}
---
+++ src/main/java/me/ryzeon/mate/utils/Utils.java
@@ -0,0 +1,11 @@
+package me.ryzeon.mate.utils;
+
+/**
+ * Created by Ryzeon
+ * Project: ProjectoUPC_Mate
+ * Date: 4/2/23 @ 21:28
+ * Twitter: @Ryzeon_ 😎
+ * Github: github.ryzeon.me
+ */
+public class Utils {
+}
