614f2af1b28c40027527ca8293b2ee5e9d02483f
Ryzeon <alex.ramon2004@gmail.com>
2023-04-02 22:51:20 -0500

    feat: lombok static import

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,8 @@ module me.ryzeon.projectoupc_mate {
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
 
+    requires static lombok; // Lombok is only required at compile time, not at runtime.
+
     opens me.ryzeon.mate to javafx.fxml;
     exports me.ryzeon.mate;
 }
