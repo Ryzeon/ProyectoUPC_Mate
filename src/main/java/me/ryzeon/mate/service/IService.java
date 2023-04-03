dafb9f2589c2f06e6cef8d9b02169819c8882d3e
Ryzeon <alex.ramon2004@gmail.com>
2023-04-02 22:51:11 -0500

    fit: adding interface for service creation

---
+++ src/main/java/me/ryzeon/mate/service/IService.java
@@ -0,0 +1,15 @@
+package me.ryzeon.mate.service;
+
+/**
+ * Created by Ryzeon
+ * Project: ProjectoUPC_Mate
+ * Date: 4/2/23 @ 21:27
+ * Twitter: @Ryzeon_ 😎
+ * Github: github.ryzeon.me
+ */
+public interface IService {
+
+    void enable();
+
+    void disable();
+}
