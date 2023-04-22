406edd880e8a5290b32c422fa5542c1464d02b10
Ryzeon <alex.ramon2004@gmail.com>
2023-04-22 05:50:17 -0500

    added stay login

--- src/main/java/me/ryzeon/mate/FlightApplication.java
+++ src/main/java/me/ryzeon/mate/FlightApplication.java
@@ -2,6 +2,7 @@ package me.ryzeon.mate;
 
 import io.github.palexdev.materialfx.css.themes.MFXThemeManager;
 import io.github.palexdev.materialfx.css.themes.Themes;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -11,12 +12,17 @@ import me.ryzeon.mate.screens.ScreenFactory;
 import me.ryzeon.mate.screens.ScreenLoader;
 import me.ryzeon.mate.service.ServiceContainer;
 import me.ryzeon.mate.services.FlightService;
+import me.ryzeon.mate.services.UserService;
 
 import java.io.IOException;
 
 public class FlightApplication {
 
     private Scene scene;
+
+    private Stage stage;
+
+    private Stage activeSubStage;
 
     private static FlightApplication instance;
 
@@ -31,23 +37,46 @@ public class FlightApplication {
     }
 
     public void start(Stage stage) throws IOException {
-        ScreenLoader screen = ScreenFactory.getInstance().loadScreenWithController(Screen.LOGIN);
-        Scene scene = new Scene(screen.loadScreen());
+        this.stage = stage;
+        // Init
+        ServiceContainer.register(SQLService.class);
+        ServiceContainer.register(FlightService.class);
+        ServiceContainer.register(UserService.class);
+        ServiceContainer.enableServices();
+
+        UserService userService = ServiceContainer.get(UserService.class);
+        ScreenLoader screen = ScreenFactory.getInstance().loadScreenWithController(
+                userService.isLoggedIn() ? Screen.PRINCIPAL_PAGE : Screen.LOGIN
+        );
+        scene = new Scene(screen.loadScreen());
         stage.initStyle(StageStyle.DECORATED);
         MFXThemeManager.addOn(scene, Themes.DEFAULT, Themes.LEGACY);
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
-
-        // Init
-        ServiceContainer.register(SQLService.class);
-        ServiceContainer.register(FlightService.class);
-        ServiceContainer.enableServices();
     }
 
-    public ScreenLoader switchTo(Screen screen)throws Exception {
+    public ScreenLoader switchTo(Screen screen) throws Exception {
         ScreenLoader screenLoader = ScreenFactory.getInstance().loadScreenWithController(screen);
-        scene.setRoot(screenLoader.loadScreen());
+        scene = new Scene(screenLoader.loadScreen());
+        MFXThemeManager.addOn(scene, Themes.DEFAULT, Themes.LEGACY);
+        stage.setScene(scene);
+        return screenLoader;
+    }
+
+    public ScreenLoader openTab(Screen screen) throws Exception {
+        ScreenLoader screenLoader = ScreenFactory.getInstance().loadScreenWithController(screen);
+        activeSubStage = new Stage();
+        activeSubStage.initOwner(stage);
+        Parent parent = screenLoader.loadScreen();
+        Scene scene = new Scene(parent);
+        MFXThemeManager.addOn(scene, Themes.DEFAULT, Themes.LEGACY);
+        activeSubStage.setScene(scene);
+        activeSubStage.show();
+        activeSubStage.setOnCloseRequest(event -> {
+            activeSubStage.close();
+            activeSubStage = null;
+        });
         return screenLoader;
     }
 
