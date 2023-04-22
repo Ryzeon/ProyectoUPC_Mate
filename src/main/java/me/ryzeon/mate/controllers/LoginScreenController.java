d3ec74dbf97addbe2004d9bd80e01a93ee9c95c1
Ryzeon <alex.ramon2004@gmail.com>
2023-04-22 05:49:46 -0500

    Handle login

--- src/main/java/me/ryzeon/mate/controllers/LoginScreenController.java
+++ src/main/java/me/ryzeon/mate/controllers/LoginScreenController.java
@@ -1,21 +1,29 @@
 package me.ryzeon.mate.controllers;
 
-import io.github.palexdev.materialfx.controls.MFXButton;
-import io.github.palexdev.materialfx.controls.MFXPasswordField;
-import io.github.palexdev.materialfx.controls.MFXTextField;
-import io.github.palexdev.materialfx.controls.MFXToggleButton;
+import io.github.palexdev.materialfx.controls.*;
+import io.github.palexdev.materialfx.validation.Constraint;
+import io.github.palexdev.materialfx.validation.MFXValidator;
+import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanExpression;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.paint.Color;
+import me.ryzeon.mate.FlightApplication;
+import me.ryzeon.mate.model.user.User;
 import me.ryzeon.mate.screens.IScreenController;
+import me.ryzeon.mate.screens.Screen;
+import me.ryzeon.mate.service.ServiceContainer;
+import me.ryzeon.mate.services.UserService;
+import me.ryzeon.mate.utils.Utils;
 
 import java.net.URL;
+import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
-
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 
 /**
  * Created by Ryzeon
@@ -26,29 +34,117 @@ import javafx.stage.Stage;
  */
 public class LoginScreenController implements IScreenController<LoginScreenController> {
 
-    public MFXToggleButton test;
+    public MFXToggleButton keepLogin;
     public MFXTextField username;
     public MFXPasswordField password;
     public MFXButton login;
-    public MFXButton login1;
-    public Label warning;
+    public MFXButton signUp;
+    public Label validatorText;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        test.setOnMouseClicked(mouseEvent -> {
-            System.out.println("Clicked");
+        // Icons
+        username.setLeadingIcon(new MFXIconWrapper("fas-user", 16, Color.web("#4D4D4D"), 30));
+        password.setLeadingIcon(new MFXIconWrapper("fas-lock", 16, Color.web("#4D4D4D"), 30));
+
+        // Validators
+        username.getValidator()
+                .constraint("Username must be at least 4 characters long", username.textProperty().length().greaterThanOrEqualTo(4));
+        username.getValidator()
+                .constraint("Username must not contain special characters",
+                        containsSpecialCharacter(username.textProperty()).not());
+        password.getValidator()
+                .constraint("Password must be at least 8 characters long", password.textProperty().length().greaterThanOrEqualTo(4));
+
+        username.setOnKeyPressed(keyEvent -> {
+            if (keyEvent.getCode().getName().equals("Enter")) {
+                password.requestFocus();
+            }
         });
+        password.setOnKeyPressed(keyEvent -> {
+            if (keyEvent.getCode().getName().equals("Enter")) {
+                login.fire();
+            }
+        });
+
+        validatorText.setTextFill(Color.RED);
+        validatorText.setPrefWidth(300);
+        validatorText.setText("");
+        validatorText.setManaged(false);
     }
 
-    public void onLoginButtonClick(ActionEvent actionEvent) {
-        VerifyLogin(username.getText(), password.getText());
-    }
 
-    private void VerifyLogin(String username, String password) {
-        if (username.equals("Gustavo") && password.equals("1234")) {
-            warning.setText("Loged in successfully");
-        } else {
-            warning.setText("Access denied: Invalid username or password");
+    BooleanExpression containsSpecialCharacter(StringProperty textProperty) {
+        String text = textProperty.get();
+        if (text == null) {
+            return Bindings.when(textProperty.isNull()).then(false).otherwise(false);
         }
+        return Bindings.createBooleanBinding(() -> text.matches("[a-zA-Z0-9@_-]+"), textProperty);
     }
 
+    boolean validate() {
+        MFXValidator validator = username.getValidator();
+        List<Constraint> constraints = validator.validate();
+        if (!constraints.isEmpty()) {
+            validatorText.setText(constraints.get(0).getMessage());
+            validatorText.setManaged(true);
+            return false;
+        }
+        validator = password.getValidator();
+        constraints = validator.validate();
+        if (!constraints.isEmpty()) {
+            validatorText.setText(constraints.get(0).getMessage());
+            validatorText.setManaged(true);
+            return false;
+        }
+        validatorText.setText("");
+        validatorText.setManaged(false);
+        return true;
+    }
+
+    @FXML
+    public void onLoginButtonClick(ActionEvent actionEvent) {
+        if (!validate()) {
+            return;
+        }
+        UserService userService = ServiceContainer.get(UserService.class);
+        Optional<User> user = userService.getUser(username.getText());
+        if (user.isEmpty()) {
+            validatorText.setText("User not found, please sign up");
+            validatorText.setManaged(true);
+            return;
+        }
+        if (password.textProperty().get().equals(userService.getNicePassword(user.get()))) {
+            if (keepLogin.isSelected()) {
+                Utils.saveLocalCredentials(user.get());
+            }
+            try {
+                FlightApplication.getInstance().switchTo(Screen.PRINCIPAL_PAGE);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            validatorText.setText("Wrong credentials.");
+            validatorText.setManaged(true);
+        }
+
+    }
+
+    @FXML
+    public void onSignUp(ActionEvent actionEvent) {
+        Platform.runLater(() -> {
+            try {
+                FlightApplication.getInstance().switchTo(Screen.SIGN_UP);
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        });
+
+    }
+
+    @FXML
+    public void onFocus(ContextMenuEvent contextMenuEvent) {
+        validate();
+    }
 }
