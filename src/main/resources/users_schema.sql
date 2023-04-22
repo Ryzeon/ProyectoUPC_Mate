6f12757285d90e14bdc9aa08a18df6f731487c95
Ryzeon <alex.ramon2004@gmail.com>
2023-04-22 05:49:08 -0500

    added user slatKey to schema

--- src/main/resources/users_schema.sql
+++ src/main/resources/users_schema.sql
@@ -5,6 +5,7 @@ CREATE TABLE USERS
     id       INT          NOT NULL AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
+    saltKey VARCHAR(255) NOT NULL,
     email    VARCHAR(255) NOT NULL,
     PRIMARY KEY (id),
     UNIQUE (username),
